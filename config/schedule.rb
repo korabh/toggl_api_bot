90e15af680d36fb453b16473cf841d7dbd376cb7
Korab Hoxha <korab-h@hotmail.com>
2017-01-25 01:15:16 +0100

    Fix timezone issue

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,15 +1,14 @@
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+# config/schedule.rb
+require "tzinfo"
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# TODO: Add timezone with :tz option
-every :weekday, :at => "11:55pm" do
+def local(time)
+  TZInfo::Timezone.get('Europe/Berlin').local_to_utc(Time.parse(time))
+end
+
+every :weekday, :at => local("11:55pm") do
   rake "toggl:summary"
 end
 
-every :sunday, :at => '11:55pm' do
+every :sunday, :at => local("11:55pm") do
   rake "toggl:weekly"
 end
