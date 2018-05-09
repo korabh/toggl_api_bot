381d430098b9a618809171cbd9081dff761e036b
Korab Hoxha <korab-h@hotmail.com>
2018-05-09 23:43:35 +0200

    Update twilio.rb

--- lib/togglev_bot/twilio.rb
+++ lib/togglev_bot/twilio.rb
@@ -4,10 +4,10 @@ module TogglevBot
   class Twilio
     def send_message(body)
       ::Twilio::REST::Client.new(
-        "ACa5eac09ec84d0ab8565e4d1af98e724a",
-        "1480787fefaa9f2c015725b0355a1b7c").messages.create(
-        from: "+16178588544",
-        to: "+38649233097",
+        "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", 
+        "your_auth_token").messages.create(
+        from: "+161",
+        to: "+38649",
         body: body
       )
     end
